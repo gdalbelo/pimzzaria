d3b8f08c8f269ff3528e9c1084d0cceb998d83f8
Prelang Builder <builder@prelang.com>
2014-07-31 18:03:00 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,4 +1,8 @@
 class PizzasController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_pizza, only: [:show, :edit, :update, :destroy]
 
   # GET /pizzas
